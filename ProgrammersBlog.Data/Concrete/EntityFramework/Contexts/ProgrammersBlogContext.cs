cbe0e8ad425d6cb97d7eaa8567a18ee5bbfa261e
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2022-02-02 20:34:18 +0300

    02.02.2022 UpdateAll

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
+{
+    public class ProgrammersBlogContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV13;Database=ProgrammersBlog;Trusted_Connection=True;ConnectTimeout=30; MultipleActiveResultSets=True;");
+        }
+    }
+}
