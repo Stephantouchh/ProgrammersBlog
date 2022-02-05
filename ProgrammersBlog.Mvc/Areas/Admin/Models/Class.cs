8c3c499fd8099cfd4f8253b6172bd98001331b8b
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2022-02-05 20:05:17 +0300

    update

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Models
+{
+    public class Class
+    {
+    }
+}
