dddf16cca65065407fdeb6e3f4b75c87650fddec
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2022-02-20 16:56:14 +0300

    20.02.2022 UpdateAll

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Mvc.Areas.Admin.Models;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticleController : Controller
+    {
+        private readonly IArticleService _articleService;
+        private readonly ICategoryService _categoryService;
+
+        public ArticleController(IArticleService articleService, ICategoryService categoryService)
+        {
+            _articleService = articleService;
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _articleService.GetAllByNonDeletedAsync();
+            if (result.ResultStatus == ResultStatus.Success) return View(result.Data);
+            return NotFound();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var result = await _categoryService.GetAllByNonDeletedAsync();
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return View(new ArticleAddViewModel
+                {
+                    Categories = result.Data.Categories
+                });
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Article article)
+        {
+            return View();
+        }
+    }
+}
